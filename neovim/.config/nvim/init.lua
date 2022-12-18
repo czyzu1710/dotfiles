8455f851a8e1e3aa002639452eee3f910fbf3008
Michał Czyż <czyzu1710@gmail.com>
2022-12-18 16:15:14 +0100

    NEOVIM | Remove unnecessary config for LuaSnip when using lsp-zero.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,7 +1,6 @@
 require('settings') -- lua/settings.lua
 require('plugins')
 require('mappings')
-require('luasnip_conf')
 require('treesitter_conf')
 require('true-zen_conf')
 require('telescope_conf')
