ffd3d624e6cd5289a4f485e3709e46aba13cba90
Michał Czyż <czyzu1710@gmail.com>
2023-07-10 13:57:33 +0200

    NEOVIM | Refactor key mappings

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -1,13 +1,10 @@
 local keymap = vim.api.nvim_set_keymap
-local default_options = {noremap = true, silent = true}
-local extra_opts = {noremap = true, expr = true, silent = true}
+local default_options = { noremap = true, silent = true }
 
-keymap("i", "jk","<ESC>", default_options)
-keymap("t", "jk","C-\\><C-n>", default_options)
-keymap("n", "<a-l>", ':tabnext<CR>', default_options)
-keymap("n", "<a-h>", ':tabprevious<CR>', default_options)
-keymap("n", "<a-f>", ':Telescope find_files hidden=true<CR>', default_options)
-keymap("n", "<a-F>", ':Telescope live_grep<CR>', default_options)
+keymap("i", "jk", "<ESC>", default_options)
+keymap("t", "jk", "C-\\><C-n>", default_options)
+keymap("n", "<A-f>", ':Telescope find_files hidden=true<CR>', default_options)
+keymap("n", "<A-F>", ':Telescope live_grep<CR>', default_options)
 keymap("n", "<leader>za", ':TZAtaraxis<CR>', default_options)
 keymap("n", "<C-s>", ':write<CR>', default_options)
 
